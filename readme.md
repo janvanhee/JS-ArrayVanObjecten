44b19c38986aa51c76bb0f5276a4ffd09a642d80
Jan Van Hee <jan.vanhee@ucll.be>
2024-11-11 13:50:51 +0100

    eerste commit (zonder opl)

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Array van objecten
+
+Deze oefening hoort bij de twee hoofdstukken over arrays in de cursussite, meer specifiek vooral bij het hoofdstuk over array van objecten.
+
+Je krijgt een kleine array gegeven met enkele student-objecten (telkens een naam en een telefoonnummer). Onder de commentaar programmeer je telkens een functie (op verschillende manieren) die het telefoonnummer dat hoort bij een naam opzoekt.
---
+++ student.js
@@ -0,0 +1,29 @@
+// Gegeven volgende array van objecten. Elk object heeft een naam en telefoonnummer.  
+
+const studenten = [
+  { naam: 'Jan', telefoonnummer: '0478254879' },
+  { naam: 'Piet', telefoonnummer: '0485123456' },
+  { naam: 'Joris', telefoonnummer: '016784512' },
+  { naam: 'Corneel', telefoonnummer: '092235689' },
+  { naam: 'Khalid', telefoonnummer: '011457893' },
+  { naam: 'Aïsha', telefoonnummer: '0465789456' }
+];
+
+// Schrijf een functie ide een naam als argument krijgt en het telefoonnummer van die persoon teruggeeft. Als de persoon niet in de array zit, geef je 'niet gevonden' terug. Test je functie in de console
+function zoekTelefoon(naam) {
+
+}
+
+console.log(zoekTelefoon('Jan')); // 0478254879
+console.log(zoekTelefoon('Katelijne')); // niet gevonden  
+
+
+
+// herschrijf de functie ook met een for...of loop
+
+
+
+
+// en tenslotte ook met foreach.
+
+
